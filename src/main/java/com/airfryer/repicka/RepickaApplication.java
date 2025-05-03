76f8e9883836bb747e91e0602cf0f52a02d9a79d
kyeah <kyeahxx19@gmail.com>
2025-05-04 00:30:36 +0900

    init project

---
+++ src/main/java/com/airfryer/repicka/RepickaApplication.java
@@ -0,0 +1,13 @@
+package com.airfryer.repicka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepickaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepickaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airfryer/repicka/RepickaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airfryer.repicka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepickaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
