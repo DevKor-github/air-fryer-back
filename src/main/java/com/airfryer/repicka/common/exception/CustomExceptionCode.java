41e8a317bd9dcd2928650ed9508ea1bfbc431509
YunJaeHoon <sleepzzz214@naver.com>
2025-05-04 16:22:21 +0900

    chore: 커스텀 예외 Enum 주석 추가 #1

--- src/main/java/com/airfryer/repicka/common/exception/CustomExceptionCode.java
+++ src/main/java/com/airfryer/repicka/common/exception/CustomExceptionCode.java
@@ -11,6 +11,6 @@ public enum CustomExceptionCode
     // 로그인 관련 예외
     EXAMPLE_EXCEPTION(HttpStatus.NOT_FOUND, "예외 예시입니다.");
 
-    private final HttpStatus httpStatus;
-    private final String message;
+    private final HttpStatus httpStatus;    // HTTP 상태 코드
+    private final String message;           // 메시지
 }
