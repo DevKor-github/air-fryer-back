68b1559b16da0adbd189b7f6bc0601e87aaadd3a
kyeah <kyeahxx19@gmail.com>
2025-05-05 22:11:21 +0900

    fix: nullable=false -> @NotNull로 수정

--- src/main/java/com/airfryer/repicka/domain/user/entity/User.java
+++ src/main/java/com/airfryer/repicka/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package com.airfryer.repicka.domain.user.entity;
 
 import com.airfryer.repicka.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,23 +17,23 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id; // 사용자 식별자
 
-    @Column(nullable = false)
+    @NotNull
     private String email; // 이메일
 
-    @Column(nullable = false)
+    @NotNull
     private String nickname; // 별명
 
-    @Column(nullable = false)
+    @NotNull
     private String login_method; // 소셜 로그인 정보
 
-    @Column(nullable = false)
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Role role; // 권한
 
-    @Column(nullable = false)
+    @NotNull
     private String profile_image_url; // 프로필 이미지 URL
 
-    @Column(nullable = false)
+    @NotNull
     private boolean is_korea_univ_verified; // 고려대 학생 인증 여부
 
     @Enumerated(EnumType.STRING)
@@ -42,9 +43,9 @@ public class User extends BaseEntity {
     private int weight; // 몸무게
     private String fcm_token; // 푸시알림 토큰
 
-    @Column(nullable = false)
+    @NotNull
     private int today_post_count; // 오늘 등록한 게시글 개수
 
-    @Column(nullable = false)
+    @NotNull
     private LocalDate last_access_date; // 마지막 접속 날짜
 }
