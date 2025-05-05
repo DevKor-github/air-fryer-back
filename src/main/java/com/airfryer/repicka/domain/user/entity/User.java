fba1b524f4718af8090ec3ebd7c2198d62674b6a
kyeah <kyeahxx19@gmail.com>
2025-05-05 22:15:01 +0900

    chore: snake_case 변수명 camelCase로 수정

--- src/main/java/com/airfryer/repicka/domain/user/entity/User.java
+++ src/main/java/com/airfryer/repicka/domain/user/entity/User.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long user_id; // 사용자 식별자
+    private Long userId; // 사용자 식별자
 
     @NotNull
     private String email; // 이메일
@@ -24,28 +24,28 @@ public class User extends BaseEntity {
     private String nickname; // 별명
 
     @NotNull
-    private String login_method; // 소셜 로그인 정보
+    private String loginMethod; // 소셜 로그인 정보
 
     @NotNull
     @Enumerated(EnumType.STRING)
     private Role role; // 권한
 
     @NotNull
-    private String profile_image_url; // 프로필 이미지 URL
+    private String profileImageUrl; // 프로필 이미지 URL
 
     @NotNull
-    private boolean is_korea_univ_verified; // 고려대 학생 인증 여부
+    private boolean isKoreaUnivVerified; // 고려대 학생 인증 여부
 
     @Enumerated(EnumType.STRING)
     private Gender gender; // 성별 (F,M)
 
     private int height; // 키
     private int weight; // 몸무게
-    private String fcm_token; // 푸시알림 토큰
+    private String fcmToken; // 푸시알림 토큰
 
     @NotNull
-    private int today_post_count; // 오늘 등록한 게시글 개수
+    private int todayPostCount; // 오늘 등록한 게시글 개수
 
     @NotNull
-    private LocalDate last_access_date; // 마지막 접속 날짜
+    private LocalDate lastAccessDate; // 마지막 접속 날짜
 }
