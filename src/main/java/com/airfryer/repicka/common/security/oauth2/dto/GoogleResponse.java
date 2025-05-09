3f5576c851b6c7d75a3838330f2ffe14c844ff31
kyeah <kyeahxx19@gmail.com>
2025-05-09 12:28:56 +0900

    feat: oauth response 전용 dto 구현

--- src/main/java/com/airfryer/repicka/common/security/oauth2/dto/GoogleResponse.java
+++ src/main/java/com/airfryer/repicka/common/security/oauth2/dto/GoogleResponse.java
@@ -1,4 +1,31 @@
 package com.airfryer.repicka.common.security.oauth2.dto;
 
-public class GoogleResponse {
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class GoogleResponse implements OAuth2Response{
+    private final Map<String, Object> attribute;
+
+
+    @Override
+    public String getProvider() {
+        return "GOOGLE";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
 }
--- src/main/java/com/airfryer/repicka/common/security/oauth2/dto/OAuth2Response.java
+++ src/main/java/com/airfryer/repicka/common/security/oauth2/dto/OAuth2Response.java
@@ -1,4 +1,12 @@
 package com.airfryer.repicka.common.security.oauth2.dto;
 
 public interface OAuth2Response {
+    //제공자 (Ex. naver, google, ...)
+    String getProvider();
+    //제공자에서 발급해주는 아이디
+    String getProviderId();
+    //사용자 이메일
+    String getEmail();
+    //사용자 이름
+    String getName();
 }
