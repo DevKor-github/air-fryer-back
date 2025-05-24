ff0b698c9eef911db768c8afe3c5916fc8d0eb19
YunJaeHoon <sleepzzz214@naver.com>
2025-05-25 00:42:36 +0900

    feat: 게시글 엔티티 구현

---
+++ src/main/java/com/airfryer/repicka/domain/post/entity/PostType.java
@@ -0,0 +1,15 @@
+package com.airfryer.repicka.domain.post.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PostType
+{
+    RENTAL("RENTAL", "대여"),
+    SALE("SALE", "판매");
+
+    private final String code;
+    private final String label;
+}
