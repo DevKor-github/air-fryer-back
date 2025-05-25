2126311dab23e1c67e374d62c7560eaef1fc408c
YunJaeHoon <sleepzzz214@naver.com>
2025-05-25 20:45:26 +0900

    fix: TradeMethod 코드 수정 및 직거래/택배거래 둘 다 가능한 선택지 추가

--- src/main/java/com/airfryer/repicka/domain/item/entity/TradeMethod.java
+++ src/main/java/com/airfryer/repicka/domain/item/entity/TradeMethod.java
@@ -7,8 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TradeMethod
 {
-    DIRECT("M", "직거래"),
-    PARCEL("S", "택배거래");
+    DIRECT("DIRECT", "직거래"),
+    PARCEL("PARCEL", "택배거래"),
+    DIRECT_AND_PARCEL("DIRECT_AND_PARCEL", "직거래 및 택배거래");
 
     private final String code;
     private final String label;
