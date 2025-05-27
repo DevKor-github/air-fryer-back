d06d03e576e056a41648c711a71b3c3cfcdc5925
kyeah <kyeahxx19@gmail.com>
2025-05-28 01:59:48 +0900

    feat: item_image 생성 service 구현

--- src/main/java/com/airfryer/repicka/domain/item_image/ItemImageService.java
+++ src/main/java/com/airfryer/repicka/domain/item_image/ItemImageService.java
@@ -1,4 +1,34 @@
 package com.airfryer.repicka.domain.item_image;
 
+import com.airfryer.repicka.domain.item.entity.Item;
+import com.airfryer.repicka.domain.item_image.entity.ItemImage;
+import com.airfryer.repicka.domain.item_image.repository.ItemImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ItemImageService {
+    private final ItemImageRepository itemImageRepository;
+
+    public List<ItemImage> saveItemImage(String[] urls, Item item) {
+        List<ItemImage> itemImages = new ArrayList<>();
+
+        for (String url: urls) {
+            ItemImage itemImage = ItemImage.builder()
+                    .item(item)
+                    .imageUrl(url)
+                    .build();
+
+            itemImage = itemImageRepository.save(itemImage);
+            itemImages.add(itemImage);
+        }
+
+        return  itemImages;
+    }
 }
