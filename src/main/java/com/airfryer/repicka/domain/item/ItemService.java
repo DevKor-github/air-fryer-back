c28bf0415215e41fe38e89b5eebd7571d03ee403
kyeah <kyeahxx19@gmail.com>
2025-05-27 20:45:33 +0900

    feat: itemRepository, itemImageRepository find 기능 구현

---
+++ src/main/java/com/airfryer/repicka/domain/item/ItemService.java
@@ -0,0 +1,4 @@
+package com.airfryer.repicka.domain.item;
+
+public class ItemService {
+}
--- src/main/java/com/airfryer/repicka/domain/item_image/repository/ItemImageRepository.java
+++ src/main/java/com/airfryer/repicka/domain/item_image/repository/ItemImageRepository.java
@@ -1,7 +1,13 @@
 package com.airfryer.repicka.domain.item_image.repository;
 
+import com.airfryer.repicka.domain.item.entity.Item;
 import com.airfryer.repicka.domain.item_image.entity.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+import java.util.List;
+
+public interface ItemImageRepository extends JpaRepository<ItemImage, Long>
+{
+    // 상품 id로 해당하는 이미지 목록 찾기
+    List<ItemImage> findByItem(Item item);
 }
