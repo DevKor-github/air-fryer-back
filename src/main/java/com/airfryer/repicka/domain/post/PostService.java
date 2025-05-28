38e76aa00f279588cbe5c3cc6727631003732ae5
kyeah <kyeahxx19@gmail.com>
2025-05-29 01:59:03 +0900

    feat: postType 복수로 입력 가능하도록 변경

--- src/main/java/com/airfryer/repicka/domain/post/PostService.java
+++ src/main/java/com/airfryer/repicka/domain/post/PostService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,20 +27,26 @@ public class PostService {
     private final ItemImageService itemImageService;
 
     public PostDetailRes createPostWithItemAndImages(CreatePostReq postDetail, User user) {
-        // 상품, 상품 이미지, 게시글 저장
+        // 상품, 상품 이미지 저장
         Item item = itemService.saveItem(postDetail.getItem());
         List<ItemImage> images = itemImageService.saveItemImage(postDetail.getImages(), item);
-        Post post = Post.builder()
-                .writer(user)
-                .item(item)
-                .postType(postDetail.getPostType())
-                .price(postDetail.getPrice())
-                .deposit(postDetail.getPostType() == PostType.RENTAL ? postDetail.getDeposit() : 0)
-                .build();
-        post = postRepository.save(post);
+
+        // 게시글 타입에 따라 저장
+        // TODO: postType을 복수로 선택하여 엔터티가 두 개 생성된 경우 둘 중 어느 화면으로 이동해야 하는지 명확히 설정 (현재는 임시 설정)
+        List<Post> posts = new ArrayList<>();
+        for (PostType postType: postDetail.getPostType()) {
+            Post post = Post.builder()
+                    .writer(user)
+                    .item(item)
+                    .postType(postType)
+                    .price(postDetail.getPrice())
+                    .deposit(postType == PostType.RENTAL ? postDetail.getDeposit() : 0)
+                    .build();
+            posts.add(postRepository.save(post));
+        }
 
         // entity 정보를 PostDetailResDto로 정제
-        PostDetailRes postDetailRes = PostDetailRes.from(user, item, post, postDetail.getImages());
+        PostDetailRes postDetailRes = PostDetailRes.from(user, item, posts.get(0), postDetail.getImages());
 
         return postDetailRes;
     }
--- src/main/java/com/airfryer/repicka/domain/post/dto/CreatePostReq.java
+++ src/main/java/com/airfryer/repicka/domain/post/dto/CreatePostReq.java
@@ -2,15 +2,24 @@ package com.airfryer.repicka.domain.post.dto;
 
 import com.airfryer.repicka.domain.item.dto.CreateItemReq;
 import com.airfryer.repicka.domain.post.entity.PostType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreatePostReq {
-    private CreateItemReq item;
-    private PostType postType;
-    private int price;
-    private int deposit = 0;
+    @NotNull(message = "상품 내용을 입력해주세요.")
+    @Valid
+    private CreateItemReq item; // 상품 관련 정보 dto
+    @NotNull(message = "게시글 타입을 입력해주세요.")
+    private PostType[] postType = new PostType[2]; // 게시글 타입 배열 (대여, 판매 복수 설정 가능)
+    @NotNull(message = "대여료 및 판매료를 입력해주세요.")
+    @Min(value = 0, message = "대여료 또는 판매값은 0원 이상이어야 합니다.")
+    private int price; // 대여료 및 판매값
+    @Min(value = 0, message = "보증금은 0원 이상이어야 합니다.")
+    private int deposit = 0; // 보증금
     // TODO: S3 연결 이후 프론트에서 이미지를 받는 형식으로 변경
-    // question: 임의로 이미지 개수 10개 설정
-    private String[] images = new String[10];
+    // question: 임의로 이미지 개수 10개 설정, 상품 이미지 한 개 이상 존재해야 하나요?
+    private String[] images = new String[10]; // 상품 이미지
 }
