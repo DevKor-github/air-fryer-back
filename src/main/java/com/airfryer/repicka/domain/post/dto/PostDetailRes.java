6c0ec97ea07b91052f839cc231b859e9b9b3b52e
kyeah <kyeahxx19@gmail.com>
2025-05-29 01:33:28 +0900

    feat: 게시글 세부 사항 Response Dto 설정

--- src/main/java/com/airfryer/repicka/domain/post/dto/PostDetailRes.java
+++ src/main/java/com/airfryer/repicka/domain/post/dto/PostDetailRes.java
@@ -1,16 +1,20 @@
 package com.airfryer.repicka.domain.post.dto;
 
 import com.airfryer.repicka.domain.item.entity.*;
+import com.airfryer.repicka.domain.post.entity.Post;
 import com.airfryer.repicka.domain.post.entity.PostType;
 import com.airfryer.repicka.domain.user.dto.BaseUserDto;
+import com.airfryer.repicka.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class CreatePostRes {
+public class PostDetailRes {
+    private Long id; // 게시글 식별자
     private BaseUserDto writer; // 게시글 올린 사용자 정보
     // question: 아래부터 state까지 상품 정보인데 BaseItemDto나 다른 이름으로 따로 빼는 게 나을까요?
+    @Builder.Default
     private ProductType[] productTypes = new ProductType[2];
     private ItemSize size;
     private String title;
@@ -23,7 +27,30 @@ public class CreatePostRes {
     private CurrentItemState state;
     private PostType postType;
     private int price;
+    @Builder.Default
     private int deposit = 0;
+    @Builder.Default
     private String[] images = new String[10];
 
+    // user, item, post, imageUrl로 PostDetailRes 반환하는 정적 팩토리 메서드
+    public static PostDetailRes from(User user, Item item, Post post, String[] images) {
+        return PostDetailRes.builder()
+                .id(post.getId())
+                .writer(BaseUserDto.from(user))
+                .productTypes(item.getProductTypes())
+                .size(item.getSize())
+                .title(item.getTitle())
+                .description(item.getDescription())
+                .color(item.getColor())
+                .quality(item.getQuality())
+                .location(item.getLocation())
+                .tradeMethod(item.getTradeMethod())
+                .canDeal(item.getCanDeal())
+                .state(item.getState())
+                .postType(post.getPostType())
+                .price(post.getPrice())
+                .deposit(post.getDeposit())
+                .images(images)
+                .build();
+    }
 }
--- src/main/java/com/airfryer/repicka/domain/user/dto/BaseUserDto.java
+++ src/main/java/com/airfryer/repicka/domain/user/dto/BaseUserDto.java
@@ -1,4 +1,24 @@
 package com.airfryer.repicka.domain.user.dto;
 
+import com.airfryer.repicka.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class BaseUserDto {
+    private Long id; // 사용자 식별자
+    private String nickname; // 사용자 별명
+    private String profileImageUrl; // 프로필 사진
+    private Boolean isKoreanUnivVerified; // 고려대 학생 인증 여부
+
+    // question: MapStruct를 적용해보는 게 나을까요?
+    public static BaseUserDto from(User user) {
+        return BaseUserDto.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfileImageUrl())
+                .isKoreanUnivVerified(user.getIsKoreaUnivVerified())
+                .build();
+    }
 }
