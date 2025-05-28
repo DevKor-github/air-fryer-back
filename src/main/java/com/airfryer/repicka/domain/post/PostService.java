9f58dc7dd28966244aa78790500f552df962ea2a
kyeah <kyeahxx19@gmail.com>
2025-05-29 01:34:05 +0900

    feat: 게시글 생성 관련 service, controller 구현

--- src/main/java/com/airfryer/repicka/domain/post/PostService.java
+++ src/main/java/com/airfryer/repicka/domain/post/PostService.java
@@ -1,7 +1,11 @@
 package com.airfryer.repicka.domain.post;
 
+import com.airfryer.repicka.domain.item.ItemService;
 import com.airfryer.repicka.domain.item.entity.Item;
+import com.airfryer.repicka.domain.item_image.ItemImageService;
+import com.airfryer.repicka.domain.item_image.entity.ItemImage;
 import com.airfryer.repicka.domain.post.dto.CreatePostReq;
+import com.airfryer.repicka.domain.post.dto.PostDetailRes;
 import com.airfryer.repicka.domain.post.entity.Post;
 import com.airfryer.repicka.domain.post.entity.PostType;
 import com.airfryer.repicka.domain.post.repository.PostRepository;
@@ -10,14 +14,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PostService {
 
     private final PostRepository postRepository;
+    private final ItemService itemService;
+    private final ItemImageService itemImageService;
 
-    public Post savePost(CreatePostReq postDetail, User user, Item item) {
+    public PostDetailRes createPostWithItemAndImages(CreatePostReq postDetail, User user) {
+        // 상품, 상품 이미지, 게시글 저장
+        Item item = itemService.saveItem(postDetail.getItem());
+        List<ItemImage> images = itemImageService.saveItemImage(postDetail.getImages(), item);
         Post post = Post.builder()
                 .writer(user)
                 .item(item)
@@ -25,10 +36,12 @@ public class PostService {
                 .price(postDetail.getPrice())
                 .deposit(postDetail.getPostType() == PostType.RENTAL ? postDetail.getDeposit() : 0)
                 .build();
-
         post = postRepository.save(post);
 
-        return post;
+        // entity 정보를 PostDetailResDto로 정제
+        PostDetailRes postDetailRes = PostDetailRes.from(user, item, post, postDetail.getImages());
+
+        return postDetailRes;
     }
 
 }
