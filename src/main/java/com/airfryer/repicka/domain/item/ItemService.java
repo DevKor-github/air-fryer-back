2dc4d33f5d179fefd647794638270e4e10580a20
kyeah <kyeahxx19@gmail.com>
2025-05-29 23:02:04 +0900

    fix: @transactional 메서드 별로 변경

--- src/main/java/com/airfryer/repicka/domain/item/ItemService.java
+++ src/main/java/com/airfryer/repicka/domain/item/ItemService.java
@@ -11,20 +11,20 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ItemService {
     private final ItemRepository itemRepository;
 
     // question: MapStruct와 set을 사용 or 정적 팩토리 메서드 or 지금 방법 뭐가 나을까요?
+    @Transactional
     public Item saveItem(CreateItemReq itemDetail) {
         Item item = Item.builder()
                 .productTypes(itemDetail.getProductTypes())
                 .size(itemDetail.getSize())
-                .title(itemDetail.getTitle())
-                .description(itemDetail.getDescription())
+                .title(itemDetail.getTitle().trim())
+                .description(itemDetail.getDescription().trim())
                 .color(itemDetail.getColor())
                 .quality(itemDetail.getQuality())
-                .location(itemDetail.getLocation())
+                .location(itemDetail.getLocation().trim())
                 .tradeMethod(itemDetail.getTradeMethod())
                 .canDeal(itemDetail.getCanDeal())
                 .state(itemDetail.getState())
--- src/main/java/com/airfryer/repicka/domain/item_image/ItemImageService.java
+++ src/main/java/com/airfryer/repicka/domain/item_image/ItemImageService.java
@@ -12,11 +12,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ItemImageService {
     private final ItemImageRepository itemImageRepository;
 
     // 다수의 상품 이미지 저장
+    @Transactional
     public List<ItemImage> saveItemImage(String[] urls, Item item) {
         List<ItemImage> itemImages = new ArrayList<>();
         // TODO: image to url로 변환을 여기에서 처리하거나 s3 서비스에서 처리
@@ -26,9 +26,10 @@ public class ItemImageService {
                     .imageUrl(url)
                     .build();
 
-            itemImage = itemImageRepository.save(itemImage);
             itemImages.add(itemImage);
         }
+
+        itemImages = itemImageRepository.saveAll(itemImages);
 
         return itemImages;
     }
