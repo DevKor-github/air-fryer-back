2c2e7c04ffed5208d1508d879a2f8bb89e674e06
kyeah <kyeahxx19@gmail.com>
2025-06-01 20:20:47 +0900

    feat: queryDSL 기반 게시글 리스트 검색 repository 구현

--- src/main/java/com/airfryer/repicka/domain/post/repository/PostRepository.java
+++ src/main/java/com/airfryer/repicka/domain/post/repository/PostRepository.java
@@ -3,5 +3,5 @@ package com.airfryer.repicka.domain.post.repository;
 import com.airfryer.repicka.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
 }
