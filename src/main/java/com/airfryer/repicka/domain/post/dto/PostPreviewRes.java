3854cfdb1a25b56d3ea1308c5459691b2796611c
kyeah <kyeahxx19@gmail.com>
2025-06-01 15:12:52 +0900

    feat: 게시글 검색과 반환 dto 구현

--- src/main/java/com/airfryer/repicka/domain/post/dto/PostPreviewRes.java
+++ src/main/java/com/airfryer/repicka/domain/post/dto/PostPreviewRes.java
@@ -1,4 +1,35 @@
 package com.airfryer.repicka.domain.post.dto;
 
+import com.airfryer.repicka.domain.item.entity.ProductType;
+import com.airfryer.repicka.domain.item_image.entity.ItemImage;
+import com.airfryer.repicka.domain.post.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class PostPreviewRes {
+    private String title; // 게시글 제목
+
+    @Builder.Default
+    private ProductType[] productTypes = new ProductType[2];
+
+    private String image; // 대표 사진
+
+    private int price; // 대여료 / 판매값
+
+    private int likeCount; // 좋아요 개수
+
+    private int chatCount; // 채팅방 개수
+
+    public static PostPreviewRes from(Post post, ItemImage itemImage) {
+        return PostPreviewRes.builder()
+                .title(post.getItem().getTitle())
+                .productTypes(post.getItem().getProductTypes())
+                .image(itemImage.getImageUrl())
+                .price(post.getPrice())
+                .likeCount(post.getLikeCount())
+                .chatCount(post.getChatCount())
+                .build();
+    }
 }
--- src/main/java/com/airfryer/repicka/domain/post/dto/SearchPostReq.java
+++ src/main/java/com/airfryer/repicka/domain/post/dto/SearchPostReq.java
@@ -3,8 +3,12 @@ package com.airfryer.repicka.domain.post.dto;
 import com.airfryer.repicka.domain.item.entity.*;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class SearchPostReq {
+    private String keyword; // 검색 키워드
+
     private ProductType productType; // 제품 타입
 
     private ItemSize size; // 제품 사이즈
@@ -14,6 +18,8 @@ public class SearchPostReq {
     private ItemQuality quality; // 제품 품질
 
     private TradeMethod tradeMethod; // 거래 방식
+
+    private LocalDateTime rentalDate; // 원하는 대여 날짜
 
     private PostOrder postOrder = PostOrder.RECENT; // 게시글 순서
 }
