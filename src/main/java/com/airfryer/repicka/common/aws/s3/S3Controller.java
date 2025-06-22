fa533d8f1bd54e3d98a2d7ed435686b8eaf1e6e7
kyeah <kyeahxx19@gmail.com>
2025-06-23 02:16:05 +0900

    feat: 이미지에서 링크로 변환 기능 구현

---
+++ src/main/java/com/airfryer/repicka/common/aws/s3/S3Controller.java
@@ -0,0 +1,34 @@
+package com.airfryer.repicka.common.aws.s3;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.airfryer.repicka.common.response.SuccessResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/s3")
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/image")
+    public ResponseEntity<SuccessResponseDto> uploadFile(@RequestParam("image") MultipartFile image)
+    {
+        String imageUrl = s3Service.uploadImage(image);
+        return ResponseEntity.ok(SuccessResponseDto.builder().message("이미지 업로드 성공").data(imageUrl).build());
+    }
+
+    @PostMapping("/images")
+    public ResponseEntity<SuccessResponseDto> uploadImages(@RequestParam("images") MultipartFile[] images)  
+    {
+        String[] imageUrls = s3Service.uploadImages(images);
+        return ResponseEntity.ok(SuccessResponseDto.builder().message("이미지 업로드 성공").data(imageUrls).build());
+    }
+}
---
+++ src/main/java/com/airfryer/repicka/common/aws/s3/S3Service.java
@@ -0,0 +1,89 @@
+package com.airfryer.repicka.common.aws.s3;
+
+import com.airfryer.repicka.common.exception.CustomException;
+import com.airfryer.repicka.common.exception.CustomExceptionCode;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    
+    private final AmazonS3 amazonS3;
+    
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+    
+    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "gif", "webp");
+    private static final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+    
+    // 단일 이미지 업로드
+    public String uploadImage(MultipartFile file) {
+        try {
+            validateFile(file);
+            
+            String fileName = "images/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            
+            // S3에 파일 업로드
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                bucket, 
+                fileName, 
+                file.getInputStream(), 
+                metadata
+            );
+            amazonS3.putObject(putObjectRequest);
+            return amazonS3.getUrl(bucket, fileName).toString();
+            
+        } catch (IOException e) {
+            throw new CustomException(CustomExceptionCode.FILE_UPLOAD_FAILED, file.getOriginalFilename());
+            
+        }
+    }
+    
+    // 다중 이미지 업로드
+    public String[] uploadImages(MultipartFile[] files) {
+        return Arrays.stream(files)
+            .map(this::uploadImage)
+            .toArray(String[]::new);
+    }
+    
+    // 파일 유효성 검사
+    private void validateFile(MultipartFile file) {
+        // 파일이 비어있는지 확인
+        if (file.isEmpty()) {
+            throw new CustomException(CustomExceptionCode.FILE_NOT_FOUND, "업로드할 파일이 없습니다");
+        }
+        
+        // 파일 크기 확인
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new CustomException(CustomExceptionCode.FILE_SIZE_EXCEEDED, 
+                String.format("파일 크기가 너무 큽니다. 파일 크기: %d bytes, 최대 허용: %d bytes", file.getSize(), MAX_FILE_SIZE));
+        }
+        
+        // 파일 확장자 확인
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null || !isValidImageExtension(originalFilename)) {
+            throw new CustomException(CustomExceptionCode.INVALID_FILE_FORMAT, 
+                "이미지 형식이 허용되지 않습니다. 허용되는 이미지 형식: " + String.join(", ", ALLOWED_EXTENSIONS));
+        }
+    }
+    
+    // 이미지 파일 확장자 검사
+    private boolean isValidImageExtension(String filename) {
+        String extension = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+        return ALLOWED_EXTENSIONS.contains(extension);
+    }
+} 
