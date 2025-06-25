6fc686282e7a0c6a11c0c0f1a93e43f9b2f9de7c
YunJaeHoon <sleepzzz214@naver.com>
2025-06-25 17:22:46 +0900

    fix: ItemRepository의 findByDisplayOrderAndItemId가 ItemImage가 아닌, Optional<ItemImage>를 반환하도록 변경

--- src/main/java/com/airfryer/repicka/domain/item_image/repository/ItemImageRepository.java
+++ src/main/java/com/airfryer/repicka/domain/item_image/repository/ItemImageRepository.java
@@ -4,11 +4,12 @@ import com.airfryer.repicka.domain.item_image.entity.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemImageRepository extends JpaRepository<ItemImage, Long>
 {
     // 상품 별 대표 이미지 찾기
-    ItemImage findByDisplayOrderAndItemId(Integer displayOrder, Long itemId);
+    Optional<ItemImage> findByDisplayOrderAndItemId(Integer displayOrder, Long itemId);
 
     // 상품 별 이미지 목록 찾기
     List<ItemImage> findAllByItemId(Long itemId);
