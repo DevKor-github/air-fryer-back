ba0bab9fd2ec7e385064409f6668febe79fa20c7
YunJaeHoon <sleepzzz214@naver.com>
2025-07-06 17:26:23 +0900

    feat: 약속 ID 리스트로 대여중 약속 변경 요청 데이터를 조회하는 메서드 구현

--- src/main/java/com/airfryer/repicka/domain/appointment/repository/UpdateInProgressAppointmentRepository.java
+++ src/main/java/com/airfryer/repicka/domain/appointment/repository/UpdateInProgressAppointmentRepository.java
@@ -13,6 +13,9 @@ public interface UpdateInProgressAppointmentRepository extends JpaRepository<Upd
     // 약속 ID로 대여중 약속 변경 요청 데이터 조회
     List<UpdateInProgressAppointment> findByAppointmentId(Long appointmentId);
 
+    // 약속 ID 리스트로 대여중 약속 변경 요청 데이터 조회
+    List<UpdateInProgressAppointment> findByAppointmentIdIn(List<Long> appointmentIds);
+
     // 약속 ID, 생성한 사용자 ID로 대여중 약속 변경 요청 데이터 조회
     Optional<UpdateInProgressAppointment> findByAppointmentIdAndCreatorId(Long appointmentId, Long creatorId);
 }
