15747c979c8bf7ff3956750bfba9dc4d7d975679
YunJaeHoon <sleepzzz214@naver.com>
2025-07-07 15:02:48 +0900

    fix: FindMyAppointmentSubject 수정

--- src/main/java/com/airfryer/repicka/domain/appointment/FindMyAppointmentSubject.java
+++ src/main/java/com/airfryer/repicka/domain/appointment/FindMyAppointmentSubject.java
@@ -1,5 +1,6 @@
 package com.airfryer.repicka.domain.appointment;
 
+import com.airfryer.repicka.domain.appointment.dto.FindMyAppointmentPageReq;
 import com.airfryer.repicka.domain.appointment.entity.Appointment;
 import com.airfryer.repicka.domain.appointment.repository.AppointmentRepository;
 import com.airfryer.repicka.domain.post.entity.PostType;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -17,33 +19,43 @@ public enum FindMyAppointmentSubject
 {
     REQUESTER("REQUESTER", "대여자(구매자)") {
         @Override
-        public Page<Appointment> findAppointmentPage(AppointmentRepository repository,
-                                                     Pageable pageable,
+        public List<Appointment> findAppointmentPage(AppointmentRepository repository,
                                                      User user,
-                                                     PostType type,
-                                                     LocalDateTime fromDate)
+                                                     FindMyAppointmentPageReq dto)
         {
-            return repository.findMyAppointmentPageAsRequester(pageable, user.getId(), type, fromDate);
+            return repository.findMyAppointmentPageAsRequester(
+                    user.getId(),
+                    dto.getType(),
+                    dto.getPeriod().calculateFromDate(LocalDateTime.now()),
+                    dto.getCursorState(),
+                    dto.getCursorDate(),
+                    dto.getCursorId(),
+                    dto.getPageSize() + 1
+            );
         }
     },
     OWNER("OWNER", "소유자") {
         @Override
-        public Page<Appointment> findAppointmentPage(AppointmentRepository repository,
-                                                     Pageable pageable,
+        public List<Appointment> findAppointmentPage(AppointmentRepository repository,
                                                      User user,
-                                                     PostType type,
-                                                     LocalDateTime fromDate)
+                                                     FindMyAppointmentPageReq dto)
         {
-            return repository.findMyAppointmentPageAsOwner(pageable, user.getId(), type, fromDate);
+            return repository.findMyAppointmentPageAsOwner(
+                    user.getId(),
+                    dto.getType(),
+                    dto.getPeriod().calculateFromDate(LocalDateTime.now()),
+                    dto.getCursorState(),
+                    dto.getCursorDate(),
+                    dto.getCursorId(),
+                    dto.getPageSize() + 1
+            );
         }
     };
 
     private final String code;
     private final String label;
 
-    public abstract Page<Appointment> findAppointmentPage(AppointmentRepository repository,
-                                                          Pageable pageable,
+    public abstract List<Appointment> findAppointmentPage(AppointmentRepository repository,
                                                           User user,
-                                                          PostType type,
-                                                          LocalDateTime fromDate);
+                                                          FindMyAppointmentPageReq dto);
 }
