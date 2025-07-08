5de56d81eaefa81e9dcb5458ef2d0263bc79d4a4
YunJaeHoon <sleepzzz214@naver.com>
2025-07-08 11:53:14 +0900

    fix: ItemLike를 다시 PostLike로 수정

--- src/main/java/com/airfryer/repicka/domain/post_like/entity/PostLike.java
+++ src/main/java/com/airfryer/repicka/domain/post_like/entity/PostLike.java
@@ -8,14 +8,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
-@Table(name = "item_like")
+@Table(name = "post_like")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class PostLike extends BaseEntity
 {
-    // 제품 좋아요 식별자
+    // 게시글 좋아요 식별자
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
