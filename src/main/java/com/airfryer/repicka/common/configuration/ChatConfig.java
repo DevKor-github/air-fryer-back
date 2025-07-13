359e9675e2ba9e9d8782dab44caa76c0d97ab13c
YunJaeHoon <sleepzzz214@naver.com>
2025-07-13 18:26:56 +0900

    feat: STOMP 및 브로커 설정 클래스 구현

---
+++ src/main/java/com/airfryer/repicka/common/configuration/ChatConfig.java
@@ -0,0 +1,33 @@
+package com.airfryer.repicka.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfig implements WebSocketMessageBrokerConfigurer
+{
+    // STOMP 설정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        // STOMP 접속 주소 URL = ws://localhost:8080/ws
+        // 프로토콜이 HTTP가 아님에 유의
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    // 브로커 설정
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry)
+    {
+        // 메시지를 발행하는 엔드포인트
+        registry.setApplicationDestinationPrefixes("/pub");
+
+        // 메시지를 구독하는 요청 엔드포인트
+        registry.enableSimpleBroker("/sub");
+    }
+}
