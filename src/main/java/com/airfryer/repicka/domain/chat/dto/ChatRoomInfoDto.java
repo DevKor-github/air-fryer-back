e91c1ec04895529e9fbcdad333ebf99a3fc44315
YunJaeHoon <sleepzzz214@naver.com>
2025-07-17 23:47:55 +0900

    fix: ChatRoomInfo 클래스명 변경 후, 다른 파일로 분리

---
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/ChatRoomInfoDto.java
@@ -0,0 +1,32 @@
+package com.airfryer.repicka.domain.chat.dto;
+
+import com.airfryer.repicka.domain.chat.entity.ChatRoom;
+import com.airfryer.repicka.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@Builder
+public class ChatRoomInfoDto
+{
+    private Long chatRoomId;            // 채팅방 ID
+    private Long myUserId;              // 나의 사용자 ID
+    private Long opponentUserId;        // 상대방의 사용자 ID
+    private Boolean isOpponentKorean;   // 상대방의 고려대 인증 여부
+    private Boolean isFinished;         // 채팅방 종료 여부
+
+    public static ChatRoomInfoDto from(ChatRoom chatRoom, User me)
+    {
+        User opponent = Objects.equals(chatRoom.getRequester().getId(), me.getId()) ? chatRoom.getOwner() : chatRoom.getRequester();
+
+        return ChatRoomInfoDto.builder()
+                .chatRoomId(chatRoom.getId())
+                .myUserId(me.getId())
+                .opponentUserId(opponent.getId())
+                .isOpponentKorean(opponent.getIsKoreaUnivVerified())
+                .isFinished(chatRoom.getIsFinished())
+                .build();
+    }
+}
--- src/main/java/com/airfryer/repicka/domain/chat/dto/EnterChatRoomRes.java
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/EnterChatRoomRes.java
@@ -11,18 +11,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import org.bson.types.ObjectId;
-import reactor.core.publisher.Flux;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class EnterChatRoomRes
 {
-    private ChatRoomInfo chatRoom;              // 채팅방 정보
+    private ChatRoomInfoDto chatRoom;              // 채팅방 정보
     private ItemInfo item;                      // 제품 정보
     private List<ChatInfo> chats;               // 채팅 정보 리스트
     private List<AppointmentInfo> appointment;  // 약속 정보 리스트
@@ -39,38 +36,13 @@ public class EnterChatRoomRes
                                       boolean chatHasNext)
     {
         return EnterChatRoomRes.builder()
-                .chatRoom(ChatRoomInfo.from(chatRoom, me))
+                .chatRoom(ChatRoomInfoDto.from(chatRoom, me))
                 .item(ItemInfo.from(chatRoom.getItem(), imageUrl))
                 .chats(chatList.stream().map(ChatInfo::from).toList())
                 .appointment(appointmentList.stream().map(AppointmentInfo::from).toList())
                 .chatCursorId(chatCursorId)
                 .chatHasNext(chatHasNext)
                 .build();
-    }
-
-    // 채팅방 정보
-    @Getter
-    @Builder(access = AccessLevel.PRIVATE)
-    private static class ChatRoomInfo
-    {
-        private Long chatRoomId;            // 채팅방 ID
-        private Long myUserId;              // 나의 사용자 ID
-        private Long opponentUserId;        // 상대방의 사용자 ID
-        private Boolean isOpponentKorean;   // 상대방의 고려대 인증 여부
-        private Boolean isFinished;         // 채팅방 종료 여부
-
-        private static ChatRoomInfo from(ChatRoom chatRoom, User me)
-        {
-            User opponent = Objects.equals(chatRoom.getRequester().getId(), me.getId()) ? chatRoom.getOwner() : chatRoom.getRequester();
-
-            return ChatRoomInfo.builder()
-                    .chatRoomId(chatRoom.getId())
-                    .myUserId(me.getId())
-                    .opponentUserId(opponent.getId())
-                    .isOpponentKorean(opponent.getIsKoreaUnivVerified())
-                    .isFinished(chatRoom.getIsFinished())
-                    .build();
-        }
     }
 
     // 제품 정보
