b4a93d6a97fa36abd77e19464c8c9dc2d64fcd92
YunJaeHoon <sleepzzz214@naver.com>
2025-07-21 15:07:44 +0900

    fix: ChatPageDto에서 채팅 메시지 dto 분리

---
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/ChatMessageDto.java
@@ -0,0 +1,24 @@
+package com.airfryer.repicka.domain.chat.dto;
+
+import com.airfryer.repicka.domain.chat.entity.Chat;
+import lombok.Builder;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+
+@Getter
+@Builder
+public class ChatMessageDto
+{
+    private ObjectId chatId;    // 채팅 ID
+    private Long userId;        // 사용자 ID
+    private String content;     // 내용
+
+    public static ChatMessageDto from(Chat chat)
+    {
+        return ChatMessageDto.builder()
+                .chatId(chat.getId())
+                .userId(chat.getUserId())
+                .content(chat.getContent())
+                .build();
+    }
+}
--- src/main/java/com/airfryer/repicka/domain/chat/dto/ChatPageDto.java
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/ChatPageDto.java
@@ -1,7 +1,6 @@
 package com.airfryer.repicka.domain.chat.dto;
 
 import com.airfryer.repicka.domain.chat.entity.Chat;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import org.bson.types.ObjectId;
@@ -12,35 +11,16 @@ import java.util.List;
 @Builder
 public class ChatPageDto
 {
-    private List<Content> contents;     // 채팅 정보 리스트
-    private ObjectId cursorId;          // 채팅: 커서 ID
-    private Boolean hasNext;            // 채팅: 다음 페이지가 존재하는가?
+    private List<ChatMessageDto> messages;  // 채팅 리스트
+    private ObjectId cursorId;              // 채팅: 커서 ID
+    private Boolean hasNext;                // 채팅: 다음 페이지가 존재하는가?
 
     public static ChatPageDto of(List<Chat> chatList, ObjectId cursorId, boolean hasNext)
     {
         return ChatPageDto.builder()
-                .contents(chatList.stream().map(Content::from).toList())
+                .messages(chatList.stream().map(ChatMessageDto::from).toList())
                 .cursorId(cursorId)
                 .hasNext(hasNext)
                 .build();
-    }
-
-    // 채팅 정보
-    @Getter
-    @Builder(access = AccessLevel.PRIVATE)
-    private static class Content
-    {
-        private ObjectId chatId;    // 채팅 ID
-        private Long userId;        // 사용자 ID
-        private String content;     // 내용
-
-        private static Content from(Chat chat)
-        {
-            return Content.builder()
-                    .chatId(chat.getId())
-                    .userId(chat.getUserId())
-                    .content(chat.getContent())
-                    .build();
-        }
     }
 }
