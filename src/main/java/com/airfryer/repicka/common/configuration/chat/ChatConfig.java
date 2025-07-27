a2496b914fd8a27870b5f164248980c543829499
YunJaeHoon <sleepzzz214@naver.com>
2025-07-27 20:57:56 +0900

    fix: 채팅방 참가자만 채팅방을 구독할 수 있도록 수정

--- src/main/java/com/airfryer/repicka/common/configuration/chat/ChatConfig.java
+++ src/main/java/com/airfryer/repicka/common/configuration/chat/ChatConfig.java
@@ -1,15 +1,20 @@
-package com.airfryer.repicka.common.configuration;
+package com.airfryer.repicka.common.configuration.chat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class ChatConfig implements WebSocketMessageBrokerConfigurer
 {
+    private final ChatRoomAccessInterceptor chatRoomAccessInterceptor;
+
     // STOMP 설정
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry)
@@ -34,5 +39,11 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer
 
         // 메시지를 구독하는 요청 엔드포인트
         registry.enableSimpleBroker("/sub");
+    }
+
+    // 구독 권한 체크 설정
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(chatRoomAccessInterceptor);
     }
 }
