4ccfc360f0afa1d32d72c95a695f4394f1e49e0d
YunJaeHoon <sleepzzz214@naver.com>
2025-08-03 18:37:22 +0900

    feat: 채팅방 ID, 사용자 ID로 채팅방 참여 정보를 조회하는 메서드 구현

--- src/main/java/com/airfryer/repicka/domain/chat/repository/ParticipateChatRoomRepository.java
+++ src/main/java/com/airfryer/repicka/domain/chat/repository/ParticipateChatRoomRepository.java
@@ -4,6 +4,11 @@ import com.airfryer.repicka.domain.chat.entity.ParticipateChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ParticipateChatRoomRepository extends JpaRepository<ParticipateChatRoom, Long> {
+public interface ParticipateChatRoomRepository extends JpaRepository<ParticipateChatRoom, Long>
+{
+    /// 채팅방 ID, 사용자 ID로 채팅방 참여 정보 조회
+    Optional<ParticipateChatRoom> findByChatRoomIdAndUserId(Long chatRoomId, Long userId);
 }
