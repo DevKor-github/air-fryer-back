4b0dbaed8bdc74ad012a1bc2c079b7ab9f7e6562
YunJaeHoon <sleepzzz214@naver.com>
2025-08-06 17:51:36 +0900

    fix: 브라우저가 비정상적으로 종료되었을 때의 EXIT 메시지 구현

---
+++ src/main/java/com/airfryer/repicka/common/configuration/chat/WebSocketEventHandler.java
@@ -0,0 +1,91 @@
+package com.airfryer.repicka.common.configuration.chat;
+
+import com.airfryer.repicka.common.security.oauth2.CustomOAuth2User;
+import com.airfryer.repicka.domain.chat.dto.message.sub.SubMessageDto;
+import com.airfryer.repicka.domain.chat.dto.message.sub.event.SubMessageEvent;
+import com.airfryer.repicka.domain.chat.entity.ChatRoom;
+import com.airfryer.repicka.domain.chat.entity.ParticipateChatRoom;
+import com.airfryer.repicka.domain.chat.repository.ChatRoomRepository;
+import com.airfryer.repicka.domain.chat.repository.ParticipateChatRoomRepository;
+import com.airfryer.repicka.domain.chat.service.MapSubscribeWithRoomManager;
+import com.airfryer.repicka.domain.chat.service.OnlineStatusManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketEventHandler
+{
+    private final ChatRoomRepository chatRoomRepository;
+    private final ParticipateChatRoomRepository participateChatRoomRepository;
+
+    private final OnlineStatusManager onlineStatusManager;
+    private final MapSubscribeWithRoomManager mapSubscribeWithRoomManager;
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @EventListener
+    public void handleSubMessage(SubMessageEvent event)
+    {
+        if(event.getUserId() == null) {
+            messagingTemplate.convertAndSend(event.getDestination(), event.getMessage());
+        } else {
+            messagingTemplate.convertAndSendToUser(event.getUserId().toString(), event.getDestination(), event.getMessage());
+        }
+    }
+
+    @EventListener
+    public void handleSessionDisconnect(SessionDisconnectEvent event)
+    {
+        String sessionId = event.getSessionId();
+        Map<String, Long> subIdToRoomMap = mapSubscribeWithRoomManager.getAllMappingsBySessionId(sessionId);
+
+        for(Map.Entry<String, Long> entry : subIdToRoomMap.entrySet())
+        {
+            StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+
+            // 채팅방 ID
+            Long chatRoomId = entry.getValue();
+
+            // 사용자 ID
+            Authentication auth = (Authentication) accessor.getUser();
+            CustomOAuth2User customOAuth2User = (CustomOAuth2User) Objects.requireNonNull(auth).getPrincipal();
+            Long userId = customOAuth2User.getUser().getId();
+
+            ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                    .orElseThrow(() -> new RuntimeException("ChatRoom not found"));
+
+            // 온라인 상태 변경 및 퇴장 메시지 전송
+            onlineStatusManager.markUserOffline(chatRoomId, userId);
+
+            // 참여 정보 가져오기
+            ParticipateChatRoom requester = participateChatRoomRepository.findByChatRoomIdAndParticipantId(chatRoomId, chatRoom.getRequester().getId()).orElseThrow();
+            ParticipateChatRoom owner = participateChatRoomRepository.findByChatRoomIdAndParticipantId(chatRoomId, chatRoom.getOwner().getId()).orElseThrow();
+
+            SubMessageDto exitMessage = SubMessageDto.createExitMessage(
+                    chatRoom,
+                    onlineStatusManager.isUserOnline(chatRoomId, chatRoom.getRequester().getId()),
+                    onlineStatusManager.isUserOnline(chatRoomId, chatRoom.getOwner().getId()),
+                    requester.getLastEnterAt(),
+                    owner.getLastEnterAt()
+            );
+
+            handleSubMessage(SubMessageEvent.builder()
+                    .userId(null)
+                    .destination("/sub/chatroom/" + chatRoomId)
+                    .message(exitMessage)
+                    .build());
+        }
+
+        // 매핑 정보 제거
+        mapSubscribeWithRoomManager.removeAllMappingsBySessionId(sessionId);
+    }
+}
--- src/main/java/com/airfryer/repicka/domain/chat/service/MapSubscribeWithRoomManager.java
+++ src/main/java/com/airfryer/repicka/domain/chat/service/MapSubscribeWithRoomManager.java
@@ -4,6 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class MapSubscribeWithRoomManager
@@ -26,11 +31,47 @@ public class MapSubscribeWithRoomManager
         return value != null ? Long.parseLong(value) : null;
     }
 
+    public Map<String, Long> getAllMappingsBySessionId(String sessionId)
+    {
+        String pattern = buildKey(sessionId, "*");
+        Set<String> keys = redisTemplate.keys(pattern);
+
+        if(keys.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, Long> result = new HashMap<>();
+
+        for(String key : keys)
+        {
+            String value = (String) redisTemplate.opsForValue().get(key);
+
+            if (value != null)
+            {
+                String[] parts = key.split(":");
+                String subId = parts[parts.length - 1];
+                result.put(subId, Long.parseLong(value));
+            }
+        }
+
+        return result;
+    }
+
     // 매핑 정보 제거
     public void removeMapping(String sessionId, String subId)
     {
         String key = buildKey(sessionId, subId);
         redisTemplate.delete(key);
+    }
+
+    public void removeAllMappingsBySessionId(String sessionId)
+    {
+        String pattern = buildKey(sessionId, "*");
+        Set<String> keys = redisTemplate.keys(pattern);
+
+        if(!keys.isEmpty()) {
+            redisTemplate.delete(keys);
+        }
     }
 
     // key 생성
