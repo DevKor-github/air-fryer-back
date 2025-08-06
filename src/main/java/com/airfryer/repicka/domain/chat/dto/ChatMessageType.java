8a7b4b19bb673881e3f61e9c140f39f2152f3bcc
YunJaeHoon <sleepzzz214@naver.com>
2025-08-06 11:50:51 +0900

    feat: 채팅 메시지 타입 enum 구현

---
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/ChatMessageType.java
@@ -0,0 +1,16 @@
+package com.airfryer.repicka.domain.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ChatMessageType
+{
+    CHAT("CHAT", "채팅"),
+    ENTER("ENTER", "채팅방 입장"),
+    EXIT("EXIT", "채팅방 퇴장");
+
+    private final String code;
+    private final String label;
+}
