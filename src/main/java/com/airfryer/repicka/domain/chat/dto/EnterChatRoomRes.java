717d6d9c63dc4fa1840c1741a45dd8cdfc6f25f8
YunJaeHoon <sleepzzz214@naver.com>
2025-08-10 14:38:39 +0900

    fix: EnterChatRoomRes는 내부 채팅방 클래스를 사용하도록 변경

--- src/main/java/com/airfryer/repicka/domain/chat/dto/EnterChatRoomRes.java
+++ src/main/java/com/airfryer/repicka/domain/chat/dto/EnterChatRoomRes.java
@@ -9,7 +9,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
@@ -29,9 +31,43 @@ public class EnterChatRoomRes
                                       boolean chatHasNext)
     {
         return EnterChatRoomRes.builder()
-                .chatRoom(ChatRoomDto.from(chatRoom, me, null, 0, isOpponentOnline, opponentParticipateChatRoom))
+                .chatRoom(ChatRoomDto.from(chatRoom, me, isOpponentOnline, opponentParticipateChatRoom))
                 .item(ItemPreviewDto.from(chatRoom.getItem(), imageUrl))
                 .chat(ChatPageDto.of(chatList, chatCursorId, chatHasNext))
                 .build();
+    }
+
+    @Getter
+    @Builder
+    static class ChatRoomDto
+    {
+        private Long chatRoomId;                    // 채팅방 ID
+        private Long myUserId;                      // 나의 사용자 ID
+        private Long opponentUserId;                // 상대방의 사용자 ID
+        private String opponentNickname;            // 상대방의 닉네임
+        private String opponentProfileImageUrl;     // 상대방의 프로필 이미지 URL
+        private Boolean isOpponentKorean;           // 상대방의 고려대 인증 여부
+        private Boolean isOpponentOnline;           // 상대방의 온라인 여부
+        private LocalDateTime opponentLastEnterAt;  // 상대방의 마지막 채팅방 입장 시점
+        private Boolean isFinished;                 // 채팅방 종료 여부
+        private LocalDateTime lastChatAt;           // 마지막 채팅 시점
+
+        private static ChatRoomDto from(ChatRoom chatRoom, User me, boolean isOpponentOnline, ParticipateChatRoom opponentParticipateChatRoom)
+        {
+            User opponent = Objects.equals(chatRoom.getRequester().getId(), me.getId()) ? chatRoom.getOwner() : chatRoom.getRequester();
+
+            return ChatRoomDto.builder()
+                    .chatRoomId(chatRoom.getId())
+                    .myUserId(me.getId())
+                    .opponentUserId(opponent.getId())
+                    .opponentNickname(opponent.getNickname())
+                    .opponentProfileImageUrl(opponent.getProfileImageUrl())
+                    .isOpponentKorean(opponent.getIsKoreaUnivVerified())
+                    .isOpponentOnline(isOpponentOnline)
+                    .opponentLastEnterAt(opponentParticipateChatRoom.getLastEnterAt())
+                    .isFinished(chatRoom.getIsFinished())
+                    .lastChatAt(chatRoom.getLastChatAt())
+                    .build();
+        }
     }
 }
