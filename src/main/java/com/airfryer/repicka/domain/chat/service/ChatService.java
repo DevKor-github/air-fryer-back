d1ebe23c8f39d5a9ce52e75dbf82acb6d86dfded
YunJaeHoon <sleepzzz214@naver.com>
2025-08-13 13:36:30 +0900

    fix: 채팅방을 이미 나갔다면 채팅방 나가기가 안되도록 조치

--- src/main/java/com/airfryer/repicka/domain/chat/service/ChatService.java
+++ src/main/java/com/airfryer/repicka/domain/chat/service/ChatService.java
@@ -282,8 +282,14 @@ public class ChatService
     {
         /// 채팅방 참여 데이터 조회
 
+        // 채팅방 참여 데이터 조회
         ParticipateChatRoom participateChatRoom = participateChatRoomRepository.findByChatRoomIdAndParticipantId(chatRoomId, user.getId())
                 .orElseThrow(() -> new CustomException(CustomExceptionCode.PARTICIPATE_CHATROOM_NOT_FOUND, null));
+
+        // 이미 채팅방을 나갔는지 확인
+        if(participateChatRoom.getHasLeftRoom()) {
+            throw new CustomException(CustomExceptionCode.ALREADY_LEFT_CHATROOM, null);
+        }
 
         /// 채팅방 조회
 
