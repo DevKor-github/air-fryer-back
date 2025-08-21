85a7bcd5858084d26f27e7b1fa878c0d0ae38efc
YunJaeHoon <sleepzzz214@naver.com>
2025-08-21 12:55:33 +0900

    feat: 차단자 ID, 피차단자 ID로 유저 차단 데이터를 조회하는 메서드 구현

--- src/main/java/com/airfryer/repicka/domain/user/repository/UserBlockRepository.java
+++ src/main/java/com/airfryer/repicka/domain/user/repository/UserBlockRepository.java
@@ -1,9 +1,14 @@
 package com.airfryer.repicka.domain.user.repository;
 
-import com.airfryer.repicka.domain.user.entity.user_report.UserReport;
+import com.airfryer.repicka.domain.user.entity.user_block.UserBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserBlockRepository extends JpaRepository<UserReport, Long> {
+public interface UserBlockRepository extends JpaRepository<UserBlock, Long>
+{
+    // 차단자 ID, 피차단자 ID로 유저 차단 데이터 조회
+    Optional<UserBlock> findByBlockerIdAndBlockedId(Long blockerId, Long blockedId);
 }
