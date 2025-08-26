7a2ed9bd6d87d46a1fc15daa57fc39c04472edf2
YunJaeHoon <sleepzzz214@naver.com>
2025-08-26 23:57:27 +0900

    hotfix: 제품 좋아요 목록 조회 시, 삭제된 제품은 조회하지 않도록 수정

--- src/main/java/com/airfryer/repicka/domain/item_like/repository/ItemLikeRepository.java
+++ src/main/java/com/airfryer/repicka/domain/item_like/repository/ItemLikeRepository.java
@@ -2,6 +2,8 @@ package com.airfryer.repicka.domain.item_like.repository;
 
 import com.airfryer.repicka.domain.item_like.entity.ItemLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,10 @@ public interface ItemLikeRepository extends JpaRepository<ItemLike, Long>
     // 제품 ID와 사용자 ID로 제품 좋아요 데이터 조회
     Optional<ItemLike> findByItemIdAndLikerId(Long itemId, Long likerId);
 
-    // 사용자 ID로 제품 좋아요 데이터 조회
-    List<ItemLike> findByLikerId(Long likerId);
+    // 사용자 ID로 삭제 되지 않은 제품 좋아요 데이터 조회
+    @Query("""
+        SELECT il FROM ItemLike il
+        WHERE il.liker.id = :likerId AND il.item.isDeleted = false
+    """)
+    List<ItemLike> findByLikerId(@Param("likerId") Long likerId);
 }
