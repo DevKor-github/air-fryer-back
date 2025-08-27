415565e3627413501d5bc5991cdee51c75bbae68
YunJaeHoon <sleepzzz214@naver.com>
2025-08-27 20:04:42 +0900

    hotfix: 채팅방 입장 시, 제품 삭제 여부도 반환

--- src/main/java/com/airfryer/repicka/domain/item/dto/ItemPreviewDto.java
+++ src/main/java/com/airfryer/repicka/domain/item/dto/ItemPreviewDto.java
@@ -25,18 +25,19 @@ public class ItemPreviewDto
     @Builder.Default
     private TransactionType[] transactionTypes = new TransactionType[2]; // 거래 타입
 
-    private String thumbnail;       // 대표 사진
-    private String title;           // 게시글 제목
-    private int rentalFee;          // 대여료
-    private int salePrice;          // 판매값
-    private int deposit;            // 보증금
-    private ItemSize size;          // 사이즈
-    private ItemColor color;         // 색상
-    private ItemQuality quality;     // 품질
+    private String thumbnail;           // 대표 사진
+    private String title;               // 게시글 제목
+    private int rentalFee;              // 대여료
+    private int salePrice;              // 판매값
+    private int deposit;                // 보증금
+    private ItemSize size;              // 사이즈
+    private ItemColor color;            // 색상
+    private ItemQuality quality;        // 품질
     private TradeMethod[] tradeMethods; // 거래 방법
-    private int likeCount;          // 좋아요 개수
-    private int chatRoomCount;      // 채팅방 개수
-    private LocalDateTime repostDate; // 게시 날짜
+    private int likeCount;              // 좋아요 개수
+    private int chatRoomCount;          // 채팅방 개수
+    private LocalDateTime repostDate;   // 게시 날짜
+    private Boolean isDeleted;          // 제품 삭제 여부
 
     public static ItemPreviewDto from(Item item, String thumbnailUrl)
     {
@@ -56,6 +57,7 @@ public class ItemPreviewDto
                 .likeCount(item.getLikeCount())
                 .chatRoomCount(item.getChatRoomCount())
                 .repostDate(item.getRepostDate())
+                .isDeleted(item.getIsDeleted())
                 .build();
     }
 }
